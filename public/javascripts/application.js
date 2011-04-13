36750d7665b6cc99e165c1dd89f4b853b1b6ab1a
Vincent Grato <vgrato@gmail.com>
2011-04-13 14:23:46 -0700

    ui stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,31 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+document.observe("dom:loaded", function() {
+  Element.hide('prereqs');
+  $$('.subsection > div:nth-child(even)').each(function(div) {
+    div.addClassName('even');
+  });
+});
+
+var show_prereq = false;
+function toggle_prereqs() {
+  $('prereqs').toggle();
+  if (show_prereq) {
+    show_prereq = false;
+    $('prereq_toggle_button').update('hide');
+  } else {
+    show_prereq = true;
+    $('prereq_toggle_button').update('show');
+  }
+}
+
+var show_resources = false;
+function toggle_resources() {
+  $('resources').toggle();
+  if (show_resources) {
+    show_resources = false;
+    $('resources_toggle_button').update('hide');
+  } else {
+    show_resources = true;
+    $('resources_toggle_button').update('show');
+  }
+}
+
