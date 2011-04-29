9986fea0953fdc488400674ffb4d9ef17727588f
Vincent Grato <vgrato@gmail.com>
2011-04-28 18:57:38 -0700

    put recent submissions on homepage

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
 
   def index
+    @submissions = Resource.all(:limit => 5, :order => 'created_at desc')
     @recent = Subject.all(:limit => 5, :order => 'created_at desc')
     @random = Subject.all(:limit => 5, :order => 'random()')
   end
